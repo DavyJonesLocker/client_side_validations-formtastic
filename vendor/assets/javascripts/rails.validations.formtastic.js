30434a52e5fb9a25c6ce3d43deaf082bcd92b79f
Brian Cardarella <bcardarella@gmail.com>
2012-01-22 03:53:10 -0500

    Added legal info to JavaScript

--- vendor/assets/javascripts/rails.validations.formtastic.js
+++ vendor/assets/javascripts/rails.validations.formtastic.js
@@ -1,3 +1,12 @@
+/*!
+ * Client Side Validations - Formtastic - v2.0.0.beta.2
+ * https://github.com/dockyard/client_side_validations-formtastic
+ *
+ * Copyright (c) 2012 DockYard, LLC
+ * Licensed under the MIT license
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
 ClientSideValidations.formBuilders['Formtastic::FormBuilder'] = {
   add: function(element, settings, message) {
     if (element.data('valid') !== false) {
