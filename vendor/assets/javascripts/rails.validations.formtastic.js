82b840630327a20522db48013a3c15b363b04d74
Brian Cardarella <bcardarella@gmail.com>
2012-01-24 04:19:44 -0500

    Generated JavaScript file

--- vendor/assets/javascripts/rails.validations.formtastic.js
+++ vendor/assets/javascripts/rails.validations.formtastic.js
@@ -1,28 +1,34 @@
-/*!
- * Client Side Validations - Formtastic - v2.0.0.beta.2
- * https://github.com/dockyard/client_side_validations-formtastic
- *
- * Copyright (c) 2012 DockYard, LLC
- * Licensed under the MIT license
- * http://www.opensource.org/licenses/mit-license.php
- */
 
-ClientSideValidations.formBuilders['Formtastic::FormBuilder'] = {
-  add: function(element, settings, message) {
-    if (element.data('valid') !== false) {
-      var wrapper = element.closest('li');
-      wrapper.addClass('error');
-      var errorElement = $('<p class="' + settings.inline_error_class + '">' + message + '</p>');
-      wrapper.append(errorElement);
-    } else {
-      element.parent().find('p.' + settings.inline_error_class).text(message);
+/*
+  Client Side Validations - Formtastic - v2.0.0.beta.2
+  https://github.com/dockyard/client_side_validations-formtastic
+
+  Copyright (c) 2012 DockYard, LLC
+  Licensed under the MIT license
+  http://www.opensource.org/licenses/mit-license.php
+*/
+
+(function() {
+
+  ClientSideValidations.formBuilders['Formtastic::FormBuilder'] = {
+    add: function(element, settings, message) {
+      var errorElement, wrapper;
+      if (element.data('valid') !== false) {
+        wrapper = element.closest('li');
+        errorElement = $('<p class="' + settings.inline_error_class + '">' + message + '</p>');
+        wrapper.addClass('error');
+        return wrapper.append(errorElement);
+      } else {
+        return element.parent().find('p.' + settings.inline_error_class).text(message);
+      }
+    },
+    remove: function(element, settings) {
+      var errorElement, wrapper;
+      wrapper = element.closest('li.error');
+      errorElement = wrapper.find('p.' + settings.inline_error_class);
+      wrapper.removeClass('error');
+      return errorElement.remove();
     }
-  },
-  remove: function(element, settings) {
-    var wrapper = element.closest('li.error');
-    wrapper.removeClass('error');
-    var errorElement = wrapper.find('p.' + settings.inline_error_class);
-    errorElement.remove();
-  }
-}
+  };
 
+}).call(this);
