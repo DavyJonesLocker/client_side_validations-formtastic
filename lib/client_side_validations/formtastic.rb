1211ef3de57f542584cd593ededfd51ebe78d801
Brian Cardarella <bcardarella@gmail.com>
2012-11-18 23:49:29 -0500

    Add support for Formtastic boolean type
    Closes #10

--- lib/client_side_validations/formtastic.rb
+++ lib/client_side_validations/formtastic.rb
@@ -2,5 +2,6 @@ module ClientSideValidations; end
 
 require 'formtastic'
 require 'client_side_validations/formtastic/form_builder'
+require 'client_side_validations/formtastic/inputs'
 require 'client_side_validations/formtastic/helpers'
 require 'client_side_validations/formtastic/engine'
--- test/formtastic/cases/test_form_helpers.rb
+++ test/formtastic/cases/test_form_helpers.rb
@@ -27,4 +27,21 @@ class ClientSideValidations::Formtastic::FormHelpersTest < ActionView::TestCase
     assert_equal expected, output_buffer, "\n\n *** If you're running Ruby 1.8 and this test fails is is most likely due to 1.8's lack of insertion order persistence with Hashes ***\n"
   end
 
+  def test_boolean
+    semantic_form_for(@post, :validate => true) do |f|
+      concat f.input(:cost, :as => :boolean)
+    end
+
+    expected = %{<form accept-charset="UTF-8" action="/posts" class="formtastic post" data-validate="true" id="new_post" method="post" novalidate="novalidate"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /></div><li class="boolean input required" id="post_cost_input"><input name="post[cost]" type="hidden" value="0" /><label class="" for="post_cost" name="post[cost]"><input id="post_cost" name="post[cost]" type="checkbox" value="1" />Cost<abbr title="required">*</abbr></label>\n\n</li></form><script>//<![CDATA[\nif(window.ClientSideValidations==undefined)window.ClientSideValidations={};if(window.ClientSideValidations.forms==undefined)window.ClientSideValidations.forms={};window.ClientSideValidations.forms['new_post'] = {"type":"Formtastic::FormBuilder","inline_error_class":"inline-errors","validators":{"post[cost]":{"presence":[{"message":"can't be blank"}]}}};\n//]]></script>}
+    assert_equal expected, output_buffer
+  end
+
+  def test_boolean_ensure_no_validate_attribute
+    semantic_form_for(@post, :validate => true) do |f|
+      concat f.input(:cost, :as => :boolean, :validate => true)
+    end
+
+    expected = %{<form accept-charset="UTF-8" action="/posts" class="formtastic post" data-validate="true" id="new_post" method="post" novalidate="novalidate"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /></div><li class="boolean input required" id="post_cost_input"><input name="post[cost]" type="hidden" value="0" /><label class="" for="post_cost" name="post[cost]" validate="true"><input id="post_cost" name="post[cost]" type="checkbox" validate="true" value="1" />Cost<abbr title="required">*</abbr></label>\n\n</li></form><script>//<![CDATA[\nif(window.ClientSideValidations==undefined)window.ClientSideValidations={};if(window.ClientSideValidations.forms==undefined)window.ClientSideValidations.forms={};window.ClientSideValidations.forms['new_post'] = {"type":"Formtastic::FormBuilder","inline_error_class":"inline-errors","validators":{"post[cost]":{"presence":[{"message":"can't be blank"}]}}};\n//]]></script>}
+    assert_equal expected, output_buffer
+  end
 end
